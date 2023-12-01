de419b7ac28e44feaaf25d1aa11012daff3c1ba0
DraganovTs <cvetomir_draganov@abv.bg>
2023-12-01 19:02:48 +0200

    init commit

---
+++ src/main/java/com/bankapplication/demo/BankApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bankapplication.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationDemoApplication.class, args);
+	}
+
+}
