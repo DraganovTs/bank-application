a36f391a06cf087bccc08d2d3134cd6cb58f756b
DraganovTs <cvetomir_draganov@abv.bg>
2023-12-02 12:46:02 +0200

    inMemory and Jdbc options explored

--- src/main/java/com/bankapplication/demo/config/ProjectSecurityConfig.java
+++ src/main/java/com/bankapplication/demo/config/ProjectSecurityConfig.java
@@ -3,7 +3,16 @@ package com.bankapplication.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,4 +34,29 @@ public class ProjectSecurityConfig {
 
     }
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
