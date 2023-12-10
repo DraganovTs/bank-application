522b5f67a538d3c28704f08f3df2c3648beb6898
DraganovTs <cvetomir_draganov@abv.bg>
2023-12-10 10:12:17 +0200

    implement keycloak

--- src/main/java/com/bankapplication/demo/controller/LoginController.java
+++ src/main/java/com/bankapplication/demo/controller/LoginController.java
@@ -2,51 +2,22 @@ package com.bankapplication.demo.controller;
 
 import com.bankapplication.demo.model.Customer;
 import com.bankapplication.demo.repository.CustomerRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+
 import java.util.List;
 
 @RestController
 public class LoginController {
 
     private final CustomerRepository customerRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+    public LoginController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
-        Customer savedCustomer = null;
-        ResponseEntity response = null;
-
-        try {
-            String hashPassword = passwordEncoder.encode(customer.getPwd());
-            customer.setPwd(hashPassword);
-            customer.setCreateDt(String.valueOf(new Date(System.currentTimeMillis())));
-            savedCustomer = customerRepository.save(customer);
-            if (savedCustomer.getId() > 0) {
-                response = ResponseEntity
-                        .status(HttpStatus.CREATED)
-                        .body("Given user details is successfully registered!");
-            }
-        } catch (Exception e) {
-            response = ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception occurred due to " + e.getMessage());
-        }
-        return response;
-    }
 
     @RequestMapping("/user")
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
